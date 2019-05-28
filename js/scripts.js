a21ce79d2bb3d4316e72cffc919f89680acc9c88
Daniel Tully <dantdesign@outlook.com>
2019-05-28 18:09:20 +1000

    refactor: loop cats and event listener
    Added each of the cats to the DOM, and a click event that changes the click count for each

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,47 @@
+// declare variables
 const catCtn = document.querySelector('.cat-ctn');
-let newCount = 0;
+let cats= [];
 
-catCtn.addEventListener('click', ()=> {
-    const counter = catCtn.querySelector('.counter');
-    newCount += 1;
+// class - cat
+class Cats {
+    constructor(name, imgSrc, altAtt) {
+        this.name = name;
+        this.imgSrc = imgSrc;
+        this.altAtt = altAtt;
+    }
+}
 
-    counter.innerText = newCount;
+// instantiate cat objects
+const cat1 = new Cats ('Tom', 'img/tom-cat.jpg', 'Tom the cat');
+const cat2 = new Cats ('Ally', 'img/ally.jpg', 'Ally the cat');
 
+cats.push(cat1);
+cats.push(cat2);
 
+window.addEventListener('DOMContentLoaded', () => {
+
+    // inject cats
+    for(cat of cats) {
+        const catBlock = document.createElement('DIV');
+
+        catBlock.innerHTML += `<!-- cat -->
+        <div class="cat-block">
+            <div class="cat-inner">
+                <div class="cat-icon">
+                    <img src="${cat.imgSrc}" alt="${cat.altAtt}" width="200" height="auto">
+                </div>
+                <div class="counter">0</div>
+            </div>
+        </div>`;
+
+        catBlock.addEventListener('click', () => {
+            const counter = catBlock.querySelector('.counter');
+            let count = parseInt(counter.innerText);
+
+            count = count += 1;
+            counter.innerText = count;
+        });
+
+        catCtn.appendChild(catBlock);
+    }
 });
