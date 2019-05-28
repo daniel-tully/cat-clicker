a4e86ef4e6343f675e048ac708ea1561063fcdbb
Daniel Tully <dantdesign@outlook.com>
2019-05-29 07:44:43 +1000

    refactor: click events on open cat
    TODO: Fix open cat clicker using closure

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,67 @@
 // declare variables
 const catCtn = document.querySelector('.cat-ctn');
-let cats= [];
 
 // class - cat
-class Cats {
+class Cat {
     constructor(name, imgSrc, altAtt) {
         this.name = name;
         this.imgSrc = imgSrc;
         this.altAtt = altAtt;
+        this.clicks = 0;
     }
 }
 
 // instantiate cat objects
-const cat1 = new Cats ('Tom', 'img/tom-cat.jpg', 'Tom the cat');
-const cat2 = new Cats ('Ally', 'img/ally.jpg', 'Ally the cat');
-
-cats.push(cat1);
-cats.push(cat2);
+const cats = [
+    cat1 = new Cat ('Tom', 'img/tom-cat.jpg', 'Tom the cat'),
+    cat2 = new Cat ('Ally', 'img/ally.jpg', 'Ally the cat'),
+    cat3 = new Cat ('Street', 'img/tom-cat.jpg', 'Street the cat'),
+    cat4 = new Cat ('Ginger', 'img/ally.jpg', 'Ginger the cat'),
+    cat5 = new Cat ('Feral', 'img/tom-cat.jpg', 'Feral the cat')
+];
 
 window.addEventListener('DOMContentLoaded', () => {
+    const listCtn = document.querySelector('.cat-list');
 
-    // inject cats
+    // inject Cat
     for(cat of cats) {
-        const catBlock = document.createElement('DIV');
+        const catLi = document.createElement('LI');
 
-        catBlock.innerHTML += `<!-- cat -->
-        <div class="cat-block">
-            <div class="cat-inner">
-                <div class="cat-icon">
-                <div class="cat-name">${cat.name}</div>
-                    <img src="${cat.imgSrc}" alt="${cat.altAtt}" width="200" height="auto">
-                </div>
-                <div class="counter">0</div>
-            </div>
-        </div>`;
+        catLi.className = "bg-light p-2 mb-1 border border-dark rounded";
 
-        catBlock.addEventListener('click', () => {
-            const counter = catBlock.querySelector('.counter');
-            let count = parseInt(counter.innerText);
+        catLi.innerText = `${cat.name}`;
 
-            count = count += 1;
-            counter.innerText = count;
-        });
+        // uses IIFE for closure with the current cat in the loop passed in at the end
+        catLi.addEventListener('click', ((thisCat) => {
+            return function() {
+                catCtn.innerHTML = `<!-- cat -->
+                <div class="cat-block container-fluid">
+                    <div class="cat-inner">
+                        <div class="cat-icon">
+                        <div class="row justify-content-between">
+                            <div class="cat-name">${thisCat.name}</div>
+                            <div class="counter">${thisCat.clicks}</div>
+                        </div>
+                        <img src="${thisCat.imgSrc}" alt="${thisCat.altAtt}" width="100%" height="auto">
+                        </div>
+                    </div>
+                </div>`;
 
-        catCtn.appendChild(catBlock);
+                console.log(thisCat.clicks);
+                // click listener for cat counter
+                catCtn.addEventListener('click', () => {
+                    const counter = catCtn.querySelector('.counter');
+
+                    console.log(thisCat.clicks);
+
+                    thisCat.clicks = thisCat.clicks += 1;
+                    counter.innerText = thisCat.clicks;
+                });
+
+                
+            }
+        })(cat));
+
+        listCtn.appendChild(catLi);
     }
 });
