139c9e664ef2ee6fae1f23952dee36cb239e6498
Daniel Tully <dantdesign@outlook.com>
2019-05-30 19:30:15 +1000

    refactor: model controller view
    refactored code from spaghetti code into separation of concerns

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,131 @@
-// declare variables
-const catCtn = document.querySelector('.cat-ctn');
+// // declare variables
+// const catCtn = document.querySelector('.cat-ctn');
 
-// class - cat
-class Cat {
-    constructor(name, imgSrc, altAtt) {
-        this.name = name;
-        this.imgSrc = imgSrc;
-        this.altAtt = altAtt;
-        this.clicks = 0;
-    }
-}
+// window.addEventListener('DOMContentLoaded', () => {
+//     const listCtn = document.querySelector('.cat-list');
 
-// instantiate cat objects
-const cats = [
-    cat1 = new Cat ('Tom', 'img/tom-cat.jpg', 'Tom the cat'),
-    cat2 = new Cat ('Ally', 'img/ally.jpg', 'Ally the cat'),
-    cat3 = new Cat ('Street', 'img/tom-cat.jpg', 'Street the cat'),
-    cat4 = new Cat ('Ginger', 'img/ally.jpg', 'Ginger the cat'),
-    cat5 = new Cat ('Feral', 'img/tom-cat.jpg', 'Feral the cat')
-];
+//     // inject Cat
+//     for(cat of cats) {
+//         const catLi = document.createElement('LI');
 
-window.addEventListener('DOMContentLoaded', () => {
-    const listCtn = document.querySelector('.cat-list');
+//         catLi.className = "li-cat bg-white p-2 rounded";
 
-    // inject Cat
-    for(cat of cats) {
-        const catLi = document.createElement('LI');
+//         catLi.innerText = `${cat.name}`;
 
-        catLi.className = "li-cat bg-white p-2 rounded";
+//         // uses IIFE for closure with the current cat in the loop passed in at the end
+//         catLi.addEventListener('click', ((thisCat) => {
+//             return function() {
+//                 catCtn.innerHTML = `<!-- cat -->
+//                 <div class="cat-block container-fluid p-0">
+//                     <div class="cat-inner">
+//                         <div class="cat-icon">
+//                         <div class="container-fluid">
+//                             <div class="row justify-content-between">
+//                                 <div class="cat-name">${thisCat.name}</div>
+//                                 <div class="counter">${thisCat.clicks}</div>
+//                             </div>
+//                         </div>
+//                         <img src="${thisCat.imgSrc}" alt="${thisCat.altAtt}" width="100%" height="auto">
+//                         </div>
+//                     </div>
+//                 </div>`;
 
-        catLi.innerText = `${cat.name}`;
+//             }
+//         })(cat));
 
-        // uses IIFE for closure with the current cat in the loop passed in at the end
-        catLi.addEventListener('click', ((thisCat) => {
-            return function() {
-                catCtn.innerHTML = `<!-- cat -->
-                <div class="cat-block container-fluid p-0">
-                    <div class="cat-inner">
-                        <div class="cat-icon">
-                        <div class="container-fluid">
-                            <div class="row justify-content-between">
-                                <div class="cat-name">${thisCat.name}</div>
-                                <div class="counter">${thisCat.clicks}</div>
-                            </div>
-                        </div>
-                        <img src="${thisCat.imgSrc}" alt="${thisCat.altAtt}" width="100%" height="auto">
-                        </div>
-                    </div>
-                </div>`;
+//         listCtn.appendChild(catLi);
+//     }
+// });
 
+
+// MCV Version
+(() => {
+
+    // model for cat list
+    const modelList = {
+        // the cats data
+        data: () => {
+            class Cat {
+                constructor(name, imgSrc, altAtt) {
+                    this.name = name;
+                    this.imgSrc = imgSrc;
+                    this.altAtt = altAtt;
+                    this.clicks = 0;
+                }
             }
-        })(cat));
-
-        listCtn.appendChild(catLi);
-    }
-});
-
-// add counter to open cat container
-catCtn.addEventListener('click', () => {
-    let openCatTitle = catCtn.querySelector('.cat-name').innerText;
-
-    for(cat of cats) {
-        if (openCatTitle === cat.name) {
-            const counter = catCtn.querySelector('.counter');
-            cat.clicks = cat.clicks += 1;
-            counter.innerText = cat.clicks;
+            return cats = [
+                cat1 = new Cat ('Tom', 'img/tom-cat.jpg', 'Tom the cat'),
+                cat2 = new Cat ('Ally', 'img/ally.jpg', 'Ally the cat'),
+                cat3 = new Cat ('Street', 'img/tom-cat.jpg', 'Street the cat'),
+                cat4 = new Cat ('Ginger', 'img/ally.jpg', 'Ginger the cat'),
+                cat5 = new Cat ('Feral', 'img/tom-cat.jpg', 'Feral the cat')
+            ];
         }
-    }
-});
+    };
+
+    // model for cat viewing area
+    const modelArea = {};
+
+    // controller
+    const controller = {
+        // get cat array data from model
+        getList: () => {
+            const cats = modelList.data();
+            return cats;
+        },
+        //start
+        init: () => {
+            view.init();
+        }
+    };
+
+    // view
+    const view = {
+        // initialise the view with list of cats
+        init: () => {
+            const cats = controller.getList();
+            const catCtn = document.querySelector('.cat-ctn');
+            const listCtn = document.querySelector('.cat-list');
+            for(cat of cats) {
+                const catLi = document.createElement('LI');
+                catLi.innerHTML = `<li class="li-cat bg-white p-2 rounded">${cat.name}</li>`;
+                // uses IIFE for closure with the current cat in the loop passed in at the end
+                catLi.addEventListener('click', ((thisCat) => {
+                    return function() {
+                        catCtn.innerHTML = `<!-- cat -->
+                        <div class="cat-block container-fluid p-0">
+                            <div class="cat-inner">
+                                <div class="cat-icon">
+                                <div class="container-fluid">
+                                    <div class="row justify-content-between">
+                                        <div class="cat-name">${thisCat.name}</div>
+                                        <div class="counter">${thisCat.clicks}</div>
+                                    </div>
+                                </div>
+                                <img src="${thisCat.imgSrc}" alt="${thisCat.altAtt}" width="100%" height="auto">
+                                </div>
+                            </div>
+                        </div>`;
+                    }
+                })(cat));
+                listCtn.appendChild(catLi);
+            }
+            // add counter to open cat container
+            catCtn.addEventListener('click', () => {
+                let openCatTitle = catCtn.querySelector('.cat-name').innerText;
+
+                for(cat of cats) {
+                    if (openCatTitle === cat.name) {
+                        const counter = catCtn.querySelector('.counter');
+                        cat.clicks = cat.clicks += 1;
+                        counter.innerText = cat.clicks;
+                    }
+                }
+            });
+        },
+        // render view 
+        showCat: () => {
+        }
+    };
+    controller.init();
+})();
