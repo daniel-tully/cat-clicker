ed4456b44df3a78b41e66ede5c071c8e55869ac6
Daniel Tully <dantdesign@outlook.com>
2019-06-01 12:04:51 +1000

    layout: admin section
    added admin button and form to update the open cat's details. TODO: finish form function and close function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,14 @@
-// // declare variables
-// const catCtn = document.querySelector('.cat-ctn');
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     const listCtn = document.querySelector('.cat-list');
-
-//     // inject Cat
-//     for(cat of cats) {
-//         const catLi = document.createElement('LI');
-
-//         catLi.className = "li-cat bg-white p-2 rounded";
-
-//         catLi.innerText = `${cat.name}`;
-
-//         // uses IIFE for closure with the current cat in the loop passed in at the end
-//         catLi.addEventListener('click', ((thisCat) => {
-//             return function() {
-//                 catCtn.innerHTML = `<!-- cat -->
-//                 <div class="cat-block container-fluid p-0">
-//                     <div class="cat-inner">
-//                         <div class="cat-icon">
-//                         <div class="container-fluid">
-//                             <div class="row justify-content-between">
-//                                 <div class="cat-name">${thisCat.name}</div>
-//                                 <div class="counter">${thisCat.clicks}</div>
-//                             </div>
-//                         </div>
-//                         <img src="${thisCat.imgSrc}" alt="${thisCat.altAtt}" width="100%" height="auto">
-//                         </div>
-//                     </div>
-//                 </div>`;
-
-//             }
-//         })(cat));
-
-//         listCtn.appendChild(catLi);
-//     }
-// });
-
-
 // MCV Version
 (() => {
 
+/* ======= Model ======= */
+
     // model for cat list
-    const modelList = {
-        // the cats data
+    const model = {
+        openCat: null,
         data: () => {
             class Cat {
-                constructor(name, imgSrc, altAtt) {
+                constructor(name, imgSrc, altAtt, open) {
                     this.name = name;
                     this.imgSrc = imgSrc;
                     this.altAtt = altAtt;
@@ -54,78 +16,115 @@
                 }
             }
             return cats = [
-                cat1 = new Cat ('Tom', 'img/tom-cat.jpg', 'Tom the cat'),
-                cat2 = new Cat ('Ally', 'img/ally.jpg', 'Ally the cat'),
-                cat3 = new Cat ('Street', 'img/tom-cat.jpg', 'Street the cat'),
-                cat4 = new Cat ('Ginger', 'img/ally.jpg', 'Ginger the cat'),
-                cat5 = new Cat ('Feral', 'img/tom-cat.jpg', 'Feral the cat')
+                cat1 = new Cat ("Tom", "img/tom-cat.jpg", "Tom the cat"),
+                cat2 = new Cat ("Ally", "img/ally.jpg", "Ally the cat"),
+                cat3 = new Cat ("Street", "img/tom-cat.jpg", "Street the cat"),
+                cat4 = new Cat ("Ginger", "img/ginger.jpg", "Ginger the cat"),
+                cat5 = new Cat ("Feral", "img/feral.jpg", "Feral the cat")
             ];
         }
     };
 
-    // model for cat viewing area
-    const modelArea = {};
+/* ======= Controller ======= */
 
     // controller
     const controller = {
         // get cat array data from model
         getList: () => {
-            const cats = modelList.data();
+            const cats = model.data();
             return cats;
         },
         //start
         init: () => {
+            controller.getList();
             view.init();
+            model.openCat = cats[0];
+            openCatView.init();
+        },
+        // get open cat
+        getCat: () => {
+            return model.openCat;
+        },
+        //set cat
+        setCat: (cat) => {
+            model.openCat = cat;
+        },
+        // increment counter
+        incrementCounter: () => {
+            model.openCat.clicks += 1;
+            openCatView.render();
         }
     };
 
-    // view
+/* ======= View ======= */
+
+    // list view
     const view = {
         // initialise the view with list of cats
         init: () => {
             const cats = controller.getList();
-            const catCtn = document.querySelector('.cat-ctn');
-            const listCtn = document.querySelector('.cat-list');
+            const listCtn = document.querySelector(".cat-list");
+            const adminBtn = document.querySelector(".admin-btn");
+            const inputForm = document.querySelector(".input-form");
+            
+            // create the list of cats
             for(cat of cats) {
-                const catLi = document.createElement('LI');
-                catLi.innerHTML = `<li class="li-cat bg-white p-2 rounded">${cat.name}</li>`;
+                const catBtn = document.createElement("BUTTON");
+                catBtn.className = "text-center btn btn-dark mb-1";
+                catBtn.innerText = `${cat.name}`;
                 // uses IIFE for closure with the current cat in the loop passed in at the end
-                catLi.addEventListener('click', ((thisCat) => {
+                catBtn.addEventListener("click", ((cat) => {
                     return function() {
-                        catCtn.innerHTML = `<!-- cat -->
-                        <div class="cat-block container-fluid p-0">
-                            <div class="cat-inner">
-                                <div class="cat-icon">
-                                <div class="container-fluid">
-                                    <div class="row justify-content-between">
-                                        <div class="cat-name">${thisCat.name}</div>
-                                        <div class="counter">${thisCat.clicks}</div>
-                                    </div>
-                                </div>
-                                <img src="${thisCat.imgSrc}" alt="${thisCat.altAtt}" width="100%" height="auto">
-                                </div>
-                            </div>
-                        </div>`;
-                    }
+                        controller.setCat(cat);
+                        openCatView.render();
+                    };
                 })(cat));
-                listCtn.appendChild(catLi);
-            }
-            // add counter to open cat container
-            catCtn.addEventListener('click', () => {
-                let openCatTitle = catCtn.querySelector('.cat-name').innerText;
 
-                for(cat of cats) {
-                    if (openCatTitle === cat.name) {
-                        const counter = catCtn.querySelector('.counter');
-                        cat.clicks = cat.clicks += 1;
-                        counter.innerText = cat.clicks;
-                    }
-                }
+                // add all the cat items to the list container
+                listCtn.appendChild(catBtn);
+            };
+
+            // open the form when admin button is clicked
+            adminBtn.addEventListener("click", () => {
+                let openCat = controller.getCat();
+
+                // show the input form
+                inputForm.classList.remove("d-none");
+
+                // add input values for open cat
+                inputForm.querySelector("#input-cat-name").value = openCat.name;
+                inputForm.querySelector("#input-cat-src").value = openCat.imgSrc;
+                inputForm.querySelector("#input-cat-clicks").value = openCat.clicks;
             });
         },
-        // render view 
-        showCat: () => {
-        }
     };
+
+    // open cat view
+    const openCatView = {
+        init: () => {
+            openCat = controller.getCat();
+            catCtn = document.querySelector(".cat-ctn");
+            catName = catCtn.querySelector(".cat-name");
+            catCounter = catCtn.querySelector(".counter");
+            catImg = catCtn.querySelector(".cat-img");
+            
+            catName.innerText = openCat.name;
+            catCounter.innerText = openCat.clicks;
+            catImg.setAttribute("src", openCat.imgSrc);
+            catImg.setAttribute("alt", openCat.altAtt);
+
+            catCtn.addEventListener("click", () => {
+                controller.incrementCounter();
+            });
+        },
+        render: () => {
+            openCat = controller.getCat();
+            this.catName.innerText = openCat.name;
+            this.catCounter.innerText = openCat.clicks;
+            this.catImg.setAttribute("src", openCat.imgSrc);
+            this.catImg.setAttribute("alt", openCat.altAtt);
+        }
+    }
+    // start the app
     controller.init();
 })();
